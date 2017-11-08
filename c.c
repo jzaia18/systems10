bf74483ca35b3da2fd2a55c44a14f9cd6e07d6da
Jake Zaia <jzaia@stuy.edu>
2017-11-08 17:01:13 -0500

    Nvm i lied

--- c.c
+++ c.c
@@ -4,17 +4,23 @@
 #include <errno.h>
 #include <signal.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 static void sighandler(int signo){
-  printf("\n%d?\nGoodbye!\n\n", signo);
-  exit(1);
+  if (signo == SIGINT) {
+    int fd = open("logfile.txt", O_APPEND | O_RDWR);
+    write(fd, "SIGINT\n", 8);
+    close(fd);
+    printf("\nGoodbye!\n\n");
+    exit(1);
+  } else if (signo == SIGUSR1)
+    printf("\n%d stopped!\n", signo);
 }
 
 int main(){
+  signal(SIGUSR1, sighandler);
+  signal(SIGINT, sighandler);
   while (1) {
-    signal(SIGTERM, sighandler);
-    signal(SIGINT, sighandler);
-
     printf("Greetings from process %d!\n", getpid());
     sleep(1);
   }
